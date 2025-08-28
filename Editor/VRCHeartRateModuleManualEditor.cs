03dc4ca9b5e69f0a36cb76e313a7b8e5811cb903
Bekosan <tankabeko@gmail.com>
2025-08-28 14:30:28 +0900

    Added: VRCHeartRateModuleManual.cs, VRCHeartRateModuleManualEditor.cs

---
+++ Editor/VRCHeartRateModuleManualEditor.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+using static BekoShop.VRCHeartRate.LocalizationManager;
+using nadena.dev.ndmf.localization;
+
+namespace BekoShop.VRCHeartRate
+{
+    [CustomEditor(typeof(VRCHeartRateModuleManual))]
+    public class OSCHeartRateModuleManualEditor : Editor
+    {
+        #if UNITY_EDITOR
+        private bool showGameObjectSettings = false;
+
+        public override void OnInspectorGUI()
+        {
+            var module = (VRCHeartRateModuleManual)target;
+
+            EditorGUILayout.HelpBox(S("oscmodule_manual.message"), MessageType.Info);
+
+            EditorGUILayout.Space();
+
+            ShowLanguageUI();
+        }
+#endif
+    }
+}
